0780a345d0171818772bdf885c53ddf2fedb6f9a
Aman G <aman.ayush.gupta@gmail.com>
2021-06-04 10:29:10 -0700

    frontend with a error + small addition of IOException

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        Encoder.encode(new File("test.txt"));
 //        Encoder.decompress(new File("test.txt.cmp"));
         // ^Commented out to show how to use the methods
